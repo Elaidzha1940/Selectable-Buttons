082bc54cde24710fc7a619e35df05eb421651e7d
Elaidzha1940 <eli080293@gmail.com>
2023-03-26 18:28:05 +0700

    Selectable Buttons

---
+++ Selectable Buttons/Selectable Buttons/Selectable_ButtonsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Selectable_ButtonsApp.swift
+//  Selectable Buttons
+//
+//  Created by Elaidzha Shchukin on 26.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct Selectable_ButtonsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
